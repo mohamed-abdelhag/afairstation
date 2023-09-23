d4bce7142ef0ca53fd2aa2dbd6e669b2fae24508
Dexxxster21 <mohameda.abdelhag@gmail.com>
2023-09-23 14:53:16 +0400

    asdasd
    main actvity

--- app/src/main/java/com/dexster/afairstation/MainActivity.kt
+++ app/src/main/java/com/dexster/afairstation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dexster.afairstation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dexster.afairstation.adapter.ItemAdapter
 import com.dexster.afairstation.data.DataSource
 import com.dexster.afairstation.databinding.ActivityMainBinding
 
@@ -13,9 +14,15 @@ class MainActivity : AppCompatActivity() {
         cajiga = ActivityMainBinding.inflate(layoutInflater)
         setContentView(cajiga.root)
 
+        // Initialize data.
+        val myDataset = DataSource().loadAffirmation()
 
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        val recyclerView = cajiga.recyclerView
+        recyclerView.adapter = ItemAdapter(this, myDataset)
 
-
+        recyclerView.setHasFixedSize(true)
 
     }
 }
